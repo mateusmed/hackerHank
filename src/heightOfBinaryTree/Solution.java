313bfb6662f0f9c8479998bd92d2c2eee0ce320a
mateusmed <mateus.medeiros46@gmail.com>
2025-01-28 17:20:13 -0300

    commit to save

--- src/heightOfBinaryTree/Solution.java
+++ src/heightOfBinaryTree/Solution.java
@@ -17,17 +17,6 @@ class Node {
     @Override
     public String toString() {
 
-        Integer valueLeft = null;
-        Integer valueRigth = null;
-
-        if(left != null){
-            valueLeft = left.data;
-        }
-
-        if(right != null){
-            valueRigth = right.data;
-        }
-
         return "{" +
                 "data=" + data +
                 ", left=" + left +
@@ -45,7 +34,19 @@ class Solution {
         Node left;
         Node right;
 
-        no input tem dizer o numero de nos e os valores de cada no
+        no input tem que dizer o numero de nos e os valores de cada no caso do codigo será montado em ordem
+        lembra lá da faculdade? existe o caso de ordem, pré-ordem e pós-ordem
+
+        7 - numero de nos
+        4 - nó 0
+        2 - nó 1
+        6 - nó 2
+        1 - nó 3
+        3 - nó 4
+        5 - nó 5
+        7 - nó 6
+
+input:
         7
         4
         2
@@ -55,19 +56,64 @@ class Solution {
         5
         7
 
+desenho para entendimento:
               4
              / \
             2   6
            / \ / \
-          1  3 5 7
+          1  3 5  7
 
+input:
+        8
+        4
+        2
+        6
+        1
+        3
+        5
+        7
+        0
+
+desenho para entendimento:
+                4
+               / \
+              2   6
+             / \ / \
+            1  3 5 7
+           /
+          0
     */
-    public static int height(Node root) {
-        // Write your code here.
-        //root.
-        System.out.println(root);
 
-        return 0;
+    static int deepNodeLevelRigth = 0;
+    static int deepNodeLevelLeft = 0;
+
+    public static Node eachNode(Node mainNode){
+
+        System.out.println("node content" + mainNode.data);
+
+        if(mainNode.left != null){
+            deepNodeLevelLeft++;
+            eachNode(mainNode.left);
+        }
+
+        if(mainNode.right != null){
+            deepNodeLevelRigth++;
+            eachNode(mainNode.right);
+        }
+
+        return mainNode;
+    }
+
+    public static int height(Node root) {
+        eachNode(root);
+
+        System.out.println("deep node left: " + deepNodeLevelLeft);
+        System.out.println("deep node rigth: " + deepNodeLevelRigth);
+
+        if(deepNodeLevelLeft > deepNodeLevelRigth){
+            return deepNodeLevelLeft;
+        }
+        return deepNodeLevelRigth;
     }
 
     public static Node insert(Node root, int data) {
